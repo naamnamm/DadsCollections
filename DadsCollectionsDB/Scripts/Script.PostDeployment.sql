247aa0070f01d9461b2dfa89b5ddbdaa6793c10c
Naam Pondpat <naam.pt@gmail.com>
2022-08-19 11:01:11 -0400

    update postdeployment scripts

--- DadsCollectionsDB/Scripts/Script.PostDeployment.sql
+++ DadsCollectionsDB/Scripts/Script.PostDeployment.sql
@@ -10,18 +10,16 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-if not exists (Select 1 from dbo.ProductTypes)
-
+if not exists (Select * from dbo.ProductTypes)
 begin
-    Insert into dbo.ProductTypes (Title,[Description])
+    Insert into dbo.ProductTypes (Title, Description)
     values ('Ceramics', 'All types of ceramics such as cups, glasses, jars, plates, and etc'),
     ('Jewelries', 'All type of accessories such as necklaces, rings and earrings made of different types of gold, diamonds, pearls, jades, and etc'),
     ('Watches', 'Different kinds of watches such as antique and automatic made of gold, silver, stainless steel, and etc'),
-    ('Other Antiques', 'All kinds of antiques')
+    ('Other Antiques', 'All kinds of antiques');
 end
 
-if not exists (Select 1 from dbo.Products)
-
+if not exists (select 1 from dbo.products)
 begin
     declare @roomId1 int;
     declare @roomId2 int;
@@ -37,5 +35,5 @@ begin
     values ('Black Ceramic Bowl', 'An antique black ceramic bowl made of sustainable material.', 50, 1, @roomId1, 'Ceramics001.jpg'),
     ('Diamond White Gold Ring', 'A half karat diamond on a 10k gold necklace', 1500, 1, @roomId2, 'Jewelries001.jpg'),
     ('Stainless Rolex', 'A Stainless dial watch with black leather arm', 1000, 1, @roomId3, 'Watches001.jpg'),
-    ('Antique Lamp', 'An antique lamp made in the 19th century', 200, 1, @roomId4, 'OtherAntiques001.jpg')
+    ('Antique Lamp', 'An antique lamp made in the 19th century', 200, 1, @roomId4, 'OtherAntiques001.jpg');
 end
