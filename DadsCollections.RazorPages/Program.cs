9de98f58f39b79a8bc43061931f130eb39d53a53
Naam Pondpat <naam.pt@gmail.com>
2022-09-30 06:15:59 -0400

    rename solution name

--- DadsCollections.RazorPages/Program.cs
+++ DadsCollections.RazorPages/Program.cs
@@ -1,7 +1,12 @@
+using DadsCollectionsLibrary.Data;
+using DadsCollectionsLibrary.DBAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IDatabaseData, SqlData>();
+builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
 
 var app = builder.Build();
 
