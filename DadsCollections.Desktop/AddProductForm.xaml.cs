3e28d04233dd79de67cc06f6f9177e7d93adca6d
Naam Pondpat <naam.pt@gmail.com>
2022-12-10 10:59:27 -0500

    able to add new product to DB from WPF app

--- DadsCollections.Desktop/AddProductForm.xaml.cs
+++ DadsCollections.Desktop/AddProductForm.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DadsCollectionsLibrary.Data;
+using DadsCollectionsLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,51 @@ namespace DadsCollections.Desktop
     /// </summary>
     public partial class AddProductForm : Window
     {
-        public AddProductForm()
+
+        // need to show blank input 
+        //insert into dbo.Products(Title, [Description], Price, Quantity, ProductTypeId, ImgName)
+        //values(@title, @description, @price, @quantity, @productTypeId, @imgName)
+
+        // pull ProductTypeId from DB
+        // show as a list
+
+        // submit Product model when click submit 
+
+
+
+        //isSold
+        private readonly IDatabaseData _db;
+
+        private ProductModel _addedProduct;
+
+        public AddProductForm(IDatabaseData db)
         {
             InitializeComponent();
+            _db = db;   
+        }
+
+        private void addProduct_Click(object sender, RoutedEventArgs e)
+        {
+            var addedProduct = new ProductModel();
+
+            //grab input value
+            //_addedProduct.Title = titleText.Text;
+            //_addedProduct.Description = descriptionText.Text;
+            //_addedProduct.Price = Convert.ToDecimal(priceText.Text);
+            //_addedProduct.Quantity = Int32.Parse(quantityText.Text);
+            //_addedProduct.ImgName = imgNameText.Text;
+            //_addedProduct.ProductTypeId = Int32.Parse(productTypeText.Text);
+
+            addedProduct.Title = titleText.Text;
+            addedProduct.Description = descriptionText.Text;
+            addedProduct.Price = Convert.ToDecimal(priceText.Text);
+            addedProduct.Quantity = Int32.Parse(quantityText.Text);
+            addedProduct.ImgName = imgNameText.Text;
+            addedProduct.ProductTypeId = Int32.Parse(productTypeText.Text);
+
+            _db.CreateProducts(addedProduct);
+
+            //if successfully added - set input text to blank and show success msg
         }
     }
 }
